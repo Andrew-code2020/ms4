8ea0a80e43497d8dc4c64082f86bc2ea99955597
Andrew Burns <burnsad@tcd.ie>
2021-03-05 16:06:10 +0000

    Removed secret keys and enabled automatic deployment to Heroku

--- a_templelean/settings.py
+++ a_templelean/settings.py
@@ -22,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '0wm3aslkm$6#jr@$d-59mrhzvi8g-50grzg4&r(8=r73^kn14='
+SECRET_KEY = os.environ.get('SECRET_KEY' '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['templeleanms4.herokuapp.com', 'localhost']
 
