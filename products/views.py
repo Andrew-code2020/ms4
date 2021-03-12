9f1ff37a8ff5023f2017af5692d89990a526fe50
Andrew Burns <burnsad@tcd.ie>
2021-03-12 17:01:00 +0000

    products template added and settings.py code, urls.py file added, models code refined

--- products/views.py
+++ products/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
-
+from .models import Product
 # Create your views here.
+
+def all_products(request):
+    """function to render classes and membership page"""
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'products/products.html', context)
