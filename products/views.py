8f2bb58f78f451c17c8a70f6cade4d578db3cb22
Andrew Burns <burnsad@tcd.ie>
2021-03-13 10:02:40 +0000

    product page developed and first product photo added to repo

--- products/views.py
+++ products/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .models import Product
 # Create your views here.
 
+
 def all_products(request):
     """function to render classes and membership page"""
     products = Product.objects.all()
@@ -11,3 +12,4 @@ def all_products(request):
     }
 
     return render(request, 'products/products.html', context)
+
