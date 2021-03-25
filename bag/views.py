9c2e6aa55f6c80174c5e7e12cf98f0a9a0c6ea2e
Andrew Burns <burnsad@tcd.ie>
2021-03-25 18:59:30 +0000

    bag view.py updated with messages. refined toasts appearance and added css for arrow

--- bag/views.py
+++ bag/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, redirect, reverse, HttpResponse
+from django.shortcuts import render, redirect, reverse, HttpResponse, get_object_or_404
 from django.contrib import messages
 
 from products.models import Product
@@ -12,7 +12,7 @@ def view_bag(request):
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
-    product = Product.objects.get(pk=item_id)
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     redirect_url = request.POST.get('redirect_url')
     days = None
@@ -24,10 +24,10 @@ def add_to_bag(request, item_id):
         if item_id in list(bag.keys()):
             if days in bag[item_id]['items_by_days'].keys():
                 bag[item_id]['items_by_days'][days] += quantity
-                messages.success(request, f'Added day {days.upper()} {product.name} total in {bag[item_id]["item_by_days"][days]}')
+                messages.success(request, f'Added day {days.upper()} {product.name} amount to {bag[item_id]["item_by_days"][days]}')
             else:
                 bag[item_id]['items_by_days'][days] = quantity
-                messages.success(request, f'Added day {days.upper()} total in {bag[item_id]}')
+                messages.success(request, f'Added day {days.upper()} {bag[item_id]} to your bag')
         else:
             bag[item_id] = {'items_by_days': {days: quantity}}
             messages.success(request, f'Added day {days.upper()} total in {bag[item_id]}')
@@ -45,7 +45,7 @@ def add_to_bag(request, item_id):
 
 def adjust_bag(request, item_id):
     """Adjust the quantity of the specified product to the specified amount"""
-
+    product = get_object_or_404(Product, pk=item_id)
     quantity = int(request.POST.get('quantity'))
     days = None
     if 'product_days' in request.POST:
@@ -55,15 +55,19 @@ def adjust_bag(request, item_id):
     if days:
         if quantity > 0:
             bag[item_id]['items_by_days'][days] = quantity
+            messages.success(request, f'Updated day {days.upper()} {product.name} amount to {bag[item_id]["item_by_days"][days]}')
         else:
             del bag[item_id]['items_by_days'][days]
             if not bag[item_id]['items_by_days']:
                 bag.pop(item_id)
+            messages.success(request, f'Removed day option {days.upper()} {product.name} from your bag')
     else:
         if quantity > 0:
             bag[item_id] = quantity
+            messages.success(request, f'Updated {product.name} total in {bag[item_id]}')
         else:
             bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
 
     request.session['bag'] = bag
     return redirect(reverse('view_bag'))
@@ -71,9 +75,9 @@ def adjust_bag(request, item_id):
 
 def remove_from_bag(request, item_id):
     """Remove the item from the shopping bag"""
-
+    product = get_object_or_404(Product, pk=item_id)
     try:
-        size = None
+        days = None
         if 'product_days' in request.POST:
             size = request.POST['product_days']
         bag = request.session.get('bag', {})
@@ -82,11 +86,14 @@ def remove_from_bag(request, item_id):
             del bag[item_id]['items_by_days'][size]
             if not bag[item_id]['items_by_days']:
                 bag.pop(item_id)
+            messages.success(request, f'Removed day option {days.upper()} {product.name} from your bag')
         else:
             bag.pop(item_id)
+            messages.success(request, f'Removed {product.name} from your bag')
 
         request.session['bag'] = bag
         return HttpResponse(status=200)
 
     except Exception as e:
+        messages.error(request, f'Error removing item: {e}')
         return HttpResponse(status=500)
