bb4096127277740ec9e6fb881dd48b3ef43ab735
Andrew Burns <burnsad@tcd.ie>
2021-03-27 11:04:54 +0000

    basic stripe functionality added. Card information box not rendering yet

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51IM7WvKk8nClvY1gJY1UFfnexe4q5ioLqF652fTPFU3tMGhARqLqxarN5WRkV6A4bnVM73HMFvxLCemJlOYJdY9c00HqQF7rjf',
+        'client_secret':'test client secret',
     }
 
     return render(request, template, context)
