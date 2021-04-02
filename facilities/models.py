2389661eca857575fef836a7e1cbfa5f04b756ad
Andrew Burns <burnsad@tcd.ie>
2021-04-02 15:18:54 +0000

    facilities model tiedied up and image css added to base.css

--- facilities/models.py
+++ facilities/models.py
@@ -17,11 +17,10 @@ class FacilityCategory(models.Model):
 
 
 class facility(models.Model):
-    category = models.ForeignKey('Category', null=True, blank=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey('FacilityCategory', null=True, blank=True, on_delete=models.SET_NULL)
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
-    rating = models.DecimalField(max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
     image = models.ImageField(null=True, blank=True)
 
