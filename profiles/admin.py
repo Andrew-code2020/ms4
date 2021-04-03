1827f42eb39317f7f6398fbdb109cf0eb882415f
Andrew Burns <burnsad@tcd.ie>
2021-04-03 11:25:12 +0000

    profile admin model refined

--- profiles/admin.py
+++ profiles/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
 
-# Register your models here.
+
+from .models import UserProfile
+
+
+class UserProfileAdmin(admin.ModelAdmin):
+
+    fields = ('user', 'default_full_name',
+              'default_email', 'default_phone_number',
+              'default_town_or_city', 'default_street_address1',
+              'default_county', 'default_street_address2',
+              'default_country' 'default_postcode',)
+
+    list_display = ('user', 'default_full_name',
+                    'default_email', 'default_phone_number',
+                    'default_town_or_city', 'default_street_address1',
+                    'default_county', 'default_street_address2',
+                    'default_country', 'default_postcode',)
+
+    ordering = ('-default_full_name',)
+
+
+admin.site.register(UserProfile, UserProfileAdmin)
