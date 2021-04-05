bf5efee8211f17b84807a6c81b36f29e9e033d99
Andrew Burns <burnsad@tcd.ie>
2021-04-05 13:03:38 +0000

    profiles admin.py registeration to Django begin. Code commented out whilst in development

--- profiles/admin.py
+++ profiles/admin.py
@@ -10,7 +10,7 @@ class UserProfileAdmin(admin.ModelAdmin):
               'default_email', 'default_phone_number',
               'default_town_or_city', 'default_street_address1',
               'default_county', 'default_street_address2',
-              'default_country' 'default_postcode',)
+              'default_country', 'default_postcode',)
 
     list_display = ('user', 'default_full_name',
                     'default_email', 'default_phone_number',
