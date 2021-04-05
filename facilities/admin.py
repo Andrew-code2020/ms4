b41264d14233f3397ac60ce90f9271675bd725e9
Andrew Burns <burnsad@tcd.ie>
2021-04-05 11:35:26 +0000

    migrations tracked and user profiles visible in database

--- facilities/admin.py
+++ facilities/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from django.contrib import admin
+
+
+#from .models import FacilityCategory, facility
+
+
+#class FacilityCategoryAdmin(admin.ModelAdmin):
+
+    #fields = ('',)
+
+    #list_display = ('',)
+
+    #ordering = ('',)
+
+
+#admin.site.register(, )
