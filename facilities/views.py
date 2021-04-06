b257e76b9e1afc0bbbd4f56a4f97d5f1a6c096c0
Andrew Burns <burnsad@tcd.ie>
2021-04-06 14:28:46 +0000

    facilties categories divided in navigation sorting fine

--- facilities/views.py
+++ facilities/views.py
@@ -6,16 +6,17 @@ from .models import facility, FacilityCategory
 def free_weights(request):
     """function to render freeweights facilities page"""
     facilities = facility.objects.all()
-    #facilitycategories = None
+    facilitycategories = None
 
-    #if request.GET:
-        #if 'facilitycategory' in request.GET:
-            #facilities = facilities.filter(category__name__in=facilitycategories)
-            #facilitycategories = FacilityCategory.objects.filter(
-                #name__in=facilitycategories)
+    if request.GET:
+        if 'facilitycategory' in request.GET:
+            facilitycategories = request.GET['facilitycategory'].split(',')
+            facilities = facilities.filter(category__name__in=facilitycategories)
+            facilitycategories = FacilityCategory.objects.filter(
+                name__in=facilitycategories)
 
     context = {
         'facilities': facilities,
-        #'facilitycategories': facilitycategories,
+        'facilitycategories': facilitycategories,
     }
     return render(request, 'facilities/freeweights.html', context)
