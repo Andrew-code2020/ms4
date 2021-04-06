763e12f33b56a947801ba10839a9219d6e1d966f
Andrew Burns <burnsad@tcd.ie>
2021-04-06 13:40:11 +0000

    Container fluid added to facilities. logic to seperate on category like project not quite functioning yet

--- facilities/views.py
+++ facilities/views.py
@@ -1,13 +1,21 @@
 from django.shortcuts import render
-
-from .models import facility, FacilityCategory 
+from django.db.models import Q
+from .models import facility, FacilityCategory
 
 
 def free_weights(request):
     """function to render freeweights facilities page"""
     facilities = facility.objects.all()
-    
+    #facilitycategories = None
+
+    #if request.GET:
+        #if 'facilitycategory' in request.GET:
+            #facilities = facilities.filter(category__name__in=facilitycategories)
+            #facilitycategories = FacilityCategory.objects.filter(
+                #name__in=facilitycategories)
+
     context = {
-        'facilities': facilities,      
+        'facilities': facilities,
+        #'facilitycategories': facilitycategories,
     }
     return render(request, 'facilities/freeweights.html', context)
