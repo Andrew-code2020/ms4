0978d461cacf29bc8d009c6bff0dad27eac52b89
Andrew Burns <burnsad@tcd.ie>
2021-04-07 10:21:24 +0000

    coach model migrations made and checked in the Django admin

--- coach/admin.py
+++ coach/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
+from .models import Coach
+
 # Register your models here.
+
+class CoachAdmin(admin.ModelAdmin):
+    list_display = (
+            'name',
+            'qualification',
+            'role',
+            'image',
+            )
+
+    ordering = ('name',)
+
+
+admin.site.register(Coach, CoachAdmin)
--- coach/models.py
+++ coach/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+# Coach Model.
+
+class Coach(models.Model):
+    name = models.CharField(max_length=254)
+    qualification = models.CharField(max_length=254)
+    role = models.CharField(max_length=254)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.name
