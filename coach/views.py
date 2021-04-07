cdfbdd73541dc6fdb2343666c5bb379e1e640a2b
Andrew Burns <burnsad@tcd.ie>
2021-04-07 11:11:39 +0000

    readme notes added on business strategy

--- coach/views.py
+++ coach/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
+from .models import Coach
 
 def coach(request):
-    """function to render coach page"""
-    
-    return render(request, 'coaches/coach.html')
+    """function to render coaches on page"""
+    coaches = Coach.objects.all()
+
+    context = {
+        coaches:'coaches',
+    }
+    return render(request, 'coaches/coach.html', context)
+
+
+
