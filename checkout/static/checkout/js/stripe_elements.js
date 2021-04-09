580773ce17f3756f4607fa7b5a1cc8e0d9d7b1be
Andrew Burns <burnsad@tcd.ie>
2021-04-09 13:14:41 +0000

    checkout buttons adjusted

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -1,4 +1,5 @@
-/*--Stripe Docs js with style refinement code copied from stripe docs adapted using boutique ado project as a guide and adapted for this project--*/
+/*--Stripe Docs js with style refinement code copied from stripe docs
+adapted using boutique ado project as a guide and adapted for this project--*/
 
 var stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 var clientSecret = $('#id_client_secret').text().slice(1, -1);
