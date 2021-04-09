cd49e6326ed842170a49a9a3fb3675fc11299ce5
Andrew Burns <burnsad@tcd.ie>
2021-04-09 16:49:24 +0000

    products app comments added to code

--- products/widgets.py
+++ products/widgets.py
@@ -2,6 +2,8 @@ from django.forms.widgets import ClearableFileInput
 from django.utils.translation import gettext_lazy as _
 
 """imported from Django Github copied from Boutique Ado project"""
+
+
 class CustomClearableFileInput(ClearableFileInput):
     clear_checkbox_label = _('Remove')
     initial_text = _('Current Image')
