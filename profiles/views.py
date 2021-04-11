92205e09b6b074dacf33f1f05f60c4bdb332c595
Andrew Burns <burnsad@tcd.ie>
2021-04-11 12:10:30 +0000

    sign up page modified to crsipy forms comments on delete profile added

--- profiles/views.py
+++ profiles/views.py
@@ -56,8 +56,10 @@ def order_history(request, order_number):
 
 @login_required
 def delete_profile(request, user_id):
-    """ Delete a class or membership from the store """
+    """ Delete profile from Temple Lean """
     profile = get_object_or_404(UserProfile, pk=user_id)
     profile.delete()
     messages.success(request, 'Profile deleted and you have been logged out')
     return redirect(reverse('account_logout'))
+
+    
